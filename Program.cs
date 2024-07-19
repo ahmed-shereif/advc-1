cfa36028a82a034c81ba619d33d15d0f701fc408
aSherif310 <asherif@tech4health.io>
2024-07-19 22:58:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace advc_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //range<double> r = new range<double>(11.1, 111.2);
+            //;
+            //Console.WriteLine(r.Length());
+
+
+            #region q2
+
+            //    ArrayList al1 = new ArrayList();
+            //    al1.Add(1);
+            //    al1.Add(2);
+            //    al1.Add(7);
+            //    al1.Add(3);
+            //    al1.Add(4);
+            //    int temp = 0;
+
+            //    for (int i = 0, j = al1.Count - 1; i < (int)al1.Count / 2; i++, j--)
+            //    {
+            //        temp = (int)al1[i];
+            //        al1[i] = al1[j];
+            //        al1[j] = temp;
+
+            //    }
+            //    foreach (var item in al1)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+
+
+            #region q3
+            //You are given a list of integers.Your task is to find and return
+            //a new list containing only the even numbers from the given list.
+            List<int> l1 = new List<int>();
+            List<int> lEven = new List<int>();
+            l1.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+
+            foreach (var item in l1)
+            {
+                if (item % 2 == 0)
+                {
+                    lEven.Add(item);
+                }
+            }
+            foreach (var item in lEven)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            #endregion
+        }
+    }
+}
---
+++ range.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advc_1
+{
+    internal class range<T> where T : IComparable<T>
+    {
+
+
+
+        public T min { get; set; }
+        public T max { get; set; }
+        public range(T min, T max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+        }
+
+        public T Length()
+        {
+            dynamic min = this.min;
+            dynamic max = this.max;
+            dynamic diffrence = max - min;
+            return diffrence;
+        }
+    }
+
+}
+
